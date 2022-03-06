3c3175f267591ce88626567eba3ac90e720563e5
Hasibul Hasan <hasibulhasan442@gmail.com>
2022-03-06 22:53:14 +0600

    Upgrade laravel 9

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,11 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
 Route::get('/', function () {
     return view('dashboard.homepage.homepage');
 });
 Route::get('/home', function () {
     return view('dashboard.homepage.homepage');
 });
+
+Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
