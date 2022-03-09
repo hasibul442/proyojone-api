6b984c5d3b4816e26b525f3a24b2c59d0b7ce7bd
Hasibul Hasan <hasibulhasan442@gmail.com>
2022-03-09 22:38:12 +0600

    service

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL', "https://proyojone-api.proyojone.xyz/public"),
+    'asset_url' => env('ASSET_URL', "https://proyojone.xyz/proyojone-api/public"),
 
     /*
     |--------------------------------------------------------------------------
