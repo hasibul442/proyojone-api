b9fbc20a8bddffbaae801d31df79828ef56df781
Hasibul Hasan <hasibulhasan442@gmail.com>
2022-03-12 00:08:40 +0600

    Login Page & Register Page

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/', function () {
-    return view('dashboard.homepage.homepage');
-});
-
-Auth::routes();
-
+Route::get('/', "App\Http\Controllers\DashboardController@index")->name('home');
 Route::get('/home', "App\Http\Controllers\DashboardController@index")->name('home');
